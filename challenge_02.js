7773ae4a8b2ff5a575c4f366c20be4c7b4c984bd
JaViKo500 <javikogutierrez64@gmail.com>
2024-11-16 23:38:42 -0500

    check values test

--- challenge_02.js
+++ challenge_02.js
@@ -4,56 +4,67 @@ const dream = [
   [4, 2, 1],
 ];
 
-function factorial(n) {
-  if (n === 0 || n === 1) {
-    return 1;
-  }
-  return n * factorial(n - 1);
-}
 
 function findSafestPath(dream = []) {
   // Code here
-  const movementsRight = dream.at(1).length -1;
-  const movementsAbove = dream.length -1 ;
-  const totalMovements = movementsAbove + movementsRight;
-  const streets = factorial( totalMovements ) / ( factorial(movementsAbove) * factorial( totalMovements - movementsAbove ) );
-  
-  console.log('<--------------- JK Challenge_02 --------------->');
-  console.log({
-    movementsAbove,
-    movementsRight,
-    streets
-  });
-  const totalStreetsValue = [];
-  for (let street = 0; street < streets; street++) {
-    let totalStreet = 0;
-    console.log(`street: ${street}`);
-    for (let index = 0; index < dream.length; index++) {
-      const currentDreamValues = dream[index];
-      console.log(`currentDreamValues: ${currentDreamValues}`);
-      for (let j = 0; j < currentDreamValues.length; j++) {
-        const dreamValue = currentDreamValues[j];
-        console.log(`dreamValue: ${dreamValue}`);
-      }
-    }
-    totalStreetsValue.push(totalStreet);
-  }
-  console.log(`Total streets${ totalStreetsValue }`);
+
+  const totalRows = {};
+  const totalColumns = {};
   for (let index = 0; index < dream.length; index++) {
     const currentDream = dream[index];
-    console.log('<--------------- JK Challenge_02 --------------->');
-    console.log(currentDream);
+    totalRows[index] = currentDream.reduce( (a,b) => a + b ); 
     for (let j = 0; j < currentDream.length; j++) {
-      const valueStreet = currentDream[j];
-      console.log('<--------------- JK Challenge_02 --------------->');
-      console.log(valueStreet);
-      totalStreet += valueStreet;
+      const dreamValue = currentDream[j];
+      totalColumns[j] = ( totalColumns[j] ?? 0 ) + dreamValue;
     }
   }
-  return 0;
+
+  const minKeyRow = getValueAndKey(totalRows);
+  const minKeyColumn = getValueAndKey(totalColumns);
+
+  let total = 0;
+  for (let index = 0; index < dream.length; index++) {
+    const currentDream = dream[index];
+    if ( minKeyRow === index.toString() ) {
+      total += currentDream.reduce( (a,b) => a + b ); 
+    }
+    for (let j = 0; j < currentDream.length; j++) {
+      const dreamValue = currentDream[j];
+      if ( minKeyColumn === j.toString() ) {
+        total += dreamValue;
+      }
+    }
+  }
+  
+  total -= dream[minKeyRow][minKeyColumn];
+
+  console.log('<--------------- JK Challenge_02 --------------->');
+  console.log({
+    totalRows, totalColumns, minKeyRow, minKeyColumn
+  });
+  return total;
+  function getValueAndKey(object) {
+    let minKey = null;
+    let minValue = Infinity;
+  
+    for (const [key, value] of Object.entries(object)) {
+      if (value < minValue) {
+        minValue = value;
+        minKey = key;
+      }
+    }
+    return minKey
+  }
 }
 
-const bestPath = findSafestPath(dream);
+// const bestPath = findSafestPath(dream);
+// const bestPath =  findSafestPath([[5, 9], [4, 2]]);
+// const bestPath =  findSafestPath([[1, 1, 1], [1, 1, 1], [1, 1, 1]]);
+const bestPath =  findSafestPath([
+  [1, 2, 3], 
+  [3, 2, 1], 
+  [4, 4, 4]
+]);
 
 console.log('<--------------- JK Challenge_02 --------------->');
 console.log(bestPath);
